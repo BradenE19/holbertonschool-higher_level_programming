56baeb4893a857f986bf9b855aab9f7386a18398
BradenE19 <6115@holbertonstudents.com>
2023-06-29 18:57:37 +0000

    SQL

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
---script lists the number of records with the same score in the table second_table
+--script lists the number of records with the same score in the table second
 SELECT 'score', COUNT(*) AS 'number'
 FROM 'second_table'
 GROUP BY 'score'
