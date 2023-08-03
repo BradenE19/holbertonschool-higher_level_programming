25831c9dccccf2ffbdf3e89040f00b8a15e73d27
BradenE19 <6115@holbertonstudents.com>
2023-08-03 13:51:51 -0500

    fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+
 const argv = process.argv;
 const fs = require('fs');
 
-fs.readFile(argv[2], 'utf8', (err, data)) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-}
+fs.readFile(argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
