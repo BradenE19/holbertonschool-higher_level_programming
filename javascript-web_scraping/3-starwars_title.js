c3d12cc6325fe2d48397489e5fd66773de270aec
BradenE19 <6115@holbertonstudents.com>
2023-08-03 16:24:22 -0500

    task3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/:id'
+const argv = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/'
 
-request(url, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-    console.log(JSON.parse(body).films);
-    }
+request(url + argv, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
 });
