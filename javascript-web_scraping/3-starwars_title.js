3b2425361868e49d922843f1ed065b58e09b87cf
BradenE19 <6115@holbertonstudents.com>
2023-08-03 16:19:22 -0500

    hope

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
-  }
+const url = 'https://swapi-api.hbtn.io/api/films/:id'
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.log(error);
+    } else {
+    console.log(JSON.parse(body).films);
+    }
 });
