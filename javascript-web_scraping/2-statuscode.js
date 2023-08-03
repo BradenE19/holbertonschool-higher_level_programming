408978465380cd6463305b72597fd93b800057aa
BradenE19 <6115@holbertonstudents.com>
2023-08-03 17:08:18 -0500

    fix

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,8 @@ const request = require('request');
 const argv = process.argv;
 
 request(argv[2], function (error, response, body) {
+  if (error) {
+    console.log('error', error);
+  }
   console.log('code:', response && response.statusCode);
 });
