a6a21f11f2374506b5e55dd859945730f93ee987
BradenE19 <6115@holbertonstudents.com>
2023-08-03 17:06:26 -0500

    semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,5 +3,5 @@ const argv = process.argv;
 const fs = require('fs');
 
 fs.writeFile(argv[2], argv[3], function (err) {
-    if (err) return console.log(err);
-  });
+  if (err) return console.log(err);
+});
--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const argv = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/'
+const url = 'https://swapi-api.hbtn.io/api/films/';
 
 request(url + argv, function (error, response, body) {
   if (error) {
